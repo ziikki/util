5e589a8d34cbac9f452f795d4097d48c86c4b292
zike-q <zike.qin@gmail.com>
2018-04-03 15:34:30 -0400

    support pip3

--- install-python.sh
+++ install-python.sh
@@ -6,6 +6,9 @@ sudo wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
 sudo tar -xvf Python-3.6.3.tgz
 cd Python-3.6.5
 sudo ./configure --enable-optimizations 
-sudo apt-get install zlib1g-dev
+
+# for pip
+sudo apt-get install  zlib1g-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 sudo make altinstall
-python3 —-version
+
+sudo pip3 install jupyter numpy scipy pandas sklearn  
